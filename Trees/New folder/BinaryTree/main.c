7fe9f50f1b9ceafb0e73a2a19b7a6fa097e0aaa5
anirudhv98 <vanirudh98@gmail.com>
2019-07-23 14:47:43 +0530

    Datastructures

--- Trees/New folder/BinaryTree/main.c
+++ Trees/New folder/BinaryTree/main.c
@@ -17,7 +17,7 @@ struct Node * create()
     }
     else
     {
-        printf("\nEnter the data to be entered,\nEnter -1 if you dont want to create a node. : ");
+        printf("\nEnter the data to be entered,Enter -1 if you dont want to create a node. : ");
         scanf("%d",&x);
         if(x!=-1)
         {
@@ -62,6 +62,46 @@ void postorder(struct Node *t)
         printf("%d ",t->data);
     }
 }
+
+int counttotalnodes(struct Node *t)
+{
+    int no=0;
+    if(t==NULL)
+        return 0;
+    else
+    {
+        no=1+counttotalnodes(t->lchild)+counttotalnodes(t->rchild);
+    }
+
+}
+
+int countleafnodes(struct Node *t)
+{
+    int no=0;
+    if(t==NULL)
+        return 0;
+    else if(t->lchild==NULL && t->rchild==NULL)
+        return 1;
+    else
+        no=countleafnodes(t->lchild)+countleafnodes(t->rchild);
+}
+
+int height(struct Node *t)
+{
+    int hl,hr;
+    if(t==NULL)
+        return 0;
+    else
+    {
+        hl=height(t->lchild);
+        hr=height(t->lchild);
+
+        if(hl>hr)
+            return hl+1;
+        else
+            return hr+1;
+    }
+}
 int main()
 {
     root=create();
@@ -71,7 +111,8 @@ int main()
     inorder(root);
     printf("\nPostorder representation : ");
     postorder(root);
-
-
+    printf("\nThe total number of nodes in tree is : %d",counttotalnodes(root));
+    printf("\nThe number of leaf nodes in tree is : %d",countleafnodes(root));
+    printf("\nThe height of the tree is : %d",height(root));
     return 0;
 }
